0ff6afd3d367b256ec3af4dd77470aa873ef1809
livia <ainezling@gmail.com>
2021-04-18 11:20:05 -0700

    fixed bug

--- services/quizzes/quizzes-service.js
+++ services/quizzes/quizzes-service.js
@@ -2,7 +2,7 @@
 const quizzesDao = require('../../daos/quizzes-dao')
 const findAllQuizzes = () => quizzesDao.findAllQuizzes()
 const findQuizById = (qid) => {
-     quizzesDao.findQuizById(qid)
+     return quizzesDao.findQuizById(qid)
 }
 module.exports = { findAllQuizzes, findQuizById }
 
